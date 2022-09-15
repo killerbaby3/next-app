f5d3a129ededdb7585a423c62b1ea372c9a6d337
Văn Thành Đạt <nhkkin76@gmail.com>
2022-09-15 17:31:23 +0700

    aaa

--- pages/section5/[pid].js
+++ pages/section5/[pid].js
@@ -2,21 +2,34 @@ import fs from 'fs.promises';
 import path from 'path';
 export default function ProductDetailPage(props){
     const { productDetail } = props;
+    if (!productDetail) {
+        return <p>Loading ....</p>;
+    }
     return <>
                 <h1>{productDetail.title}</h1>
                 <p>{productDetail.description}</p>
             </>
 }
 
-export async function getStaticProps(context){
-    const { params } = context;
-    const pId = params.pid;
-
+async function getData(){
     const filePath = path.join(process.cwd(),'data','dummy-backend.json');
     const jData = await fs.readFile(filePath);
     const data = JSON.parse(jData);
+    return data;
+}
 
+export async function getStaticProps(context){
+    const data = await getData();
+    const { params } = context;
+    const pId = params.pid;
     const p = data.products.find(p => p.id == pId);
+
+    if (!p) {
+        return {
+            notFound:true
+        };
+    }
+
     return {
         props : {
             productDetail : p
@@ -25,19 +38,13 @@ export async function getStaticProps(context){
 }
 
 export async function getStaticPaths(){
-    const filePath = path.join(process.cwd(),'data','dummy-backend.json');
-    const jData = await fs.readFile(filePath);
-    const data = JSON.parse(jData);
+    const data = await getData();
     let paths = [];
     data.products.map(function(p){
         paths.push({params:{pid : p.id}});
     });
     return {
-        // paths : [
-        //     { params : { pid : 'p1' } },
-        //     { params : { pid : 'p2' } }
-        // ],
         paths : paths,
-        fallback: false
+        fallback: true
     };
 }
